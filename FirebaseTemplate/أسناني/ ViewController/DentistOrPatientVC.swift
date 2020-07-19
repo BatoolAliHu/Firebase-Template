9de3cb0feac0f47256184d2bdf9c214f876ee87d
BatoolAliHussain <ba.a.e.hu@gmail.com>
2020-07-19 22:02:45 +0300

    44
    44

--- FirebaseTemplate/أسناني/ ViewController/DentistOrPatientVC.swift
+++ FirebaseTemplate/أسناني/ ViewController/DentistOrPatientVC.swift
@@ -20,6 +20,8 @@ class DentistOrPatientVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    
     /*
      // MARK: - Navigation
      
--- FirebaseTemplate/أسناني/ ViewController/ViewController.swift
+++ FirebaseTemplate/أسناني/ ViewController/ViewController.swift
@@ -9,18 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var signIn: UIButton!
-   
-    @IBOutlet weak var signUP: UIButton!
+    
+    
+    @IBAction func batool(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            performSegue(withIdentifier: "تسجيل دخول", sender: nil)
+        case 1:
+            performSegue(withIdentifier: "دخول", sender: nil)
+        default:
+            "Data Not Found"
+        }
+        
+    }
     
     
     override func viewDidLoad() {
-   
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-   
+    
+    
 }
+
 
